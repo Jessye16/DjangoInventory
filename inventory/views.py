a5fcc548b0e5838b803f166af624ccce704b6819
formation <formation@django-formation>
2017-04-19 10:05:02 +0200

    Create Update, Delete, Detail views

--- inventory/views.py
+++ inventory/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from inventory.models import Product
 from django.views.generic import CreateView
 from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
 from inventory.forms import ProductCreateForm
 
 
@@ -16,6 +19,22 @@ class ProductCreateView(CreateView):
     model = Product
     form_class = ProductCreateForm
     template_name = "inventory/product-create.html"
+
+class ProductDetailView(DetailView):
+    model = Product
+    context_object_name = "details"
+    template_name = "inventory/product-detail.html"
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductCreateForm
+    template_name = "inventory/product-update.html"
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    form_class = ProductCreateForm
+    template_name = "inventory/product-delete.html"
+    success_url = '/'
 
     def get_initial(self):
         return {'modified_by': self.request.user.member}
